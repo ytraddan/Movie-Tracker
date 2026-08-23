f0ed3f4e3b54014a0d35be5095020a1c44446e18
ytraddan <1nightshelter@gmail.com>
2026-08-23 23:23:45 +0300

    add main page with basic api integration and set up image handling in next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://image.tmdb.org/**")],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,64 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+const AUTH_TOKEN = process.env.TMDB_API_READ_ACCESS_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+interface Movie {
+  id: string;
+  title: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+}
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${AUTH_TOKEN}`,
+  },
+};
 
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/movie/popular?language=en-US&page=1`, options)
+      .then((res) => res.json())
+      .then((res) => setMovies(res.results))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!movies) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <ul className={styles.ul}>
+          {movies.map((movie) => (
+            <li key={movie.id} className={styles.il}>
+              <Image
+                loading="lazy"
+                alt={movie.title}
+                src={`${BASE_IMAGE_URL}${movie.poster_path}`}
+                width={240}
+                height={360}
+              />
+              <p className={styles.p}>
+                <span>{movie.title}</span>
+                <span>{movie.release_date}</span>
+                <span>{movie.vote_average}</span>
+              </p>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
