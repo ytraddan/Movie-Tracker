b6da83977548ffe482dc7d06a2fbc3b58914cab3
ytraddan <1nightshelter@gmail.com>
2026-09-04 22:10:05 +0300

    add favicon and logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,21 @@ import Link from "next/link";
 import styles from "./header.module.css";
 import SearchBar from "@/components/searchBar/SearchBar";
 import FavoriteButton from "@/components/favoriteButton/FavoriteButton";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">Movie Tracker</Link>
+      <Link href="/" className={styles.title}>
+        <Image
+          className={styles.logo}
+          src="/movie.svg"
+          alt="Movie icon"
+          width={24}
+          height={24}
+        />
+        <span>Movie Tracker</span>
+      </Link>
       <SearchBar />
       <FavoriteButton />
     </header>
