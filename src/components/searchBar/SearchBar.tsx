6c22ed03682848dc52b8ea525c1fa2469332b0ad
ytraddan <1nightshelter@gmail.com>
2026-09-15 13:25:10 +0300

    style search bar

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,5 +1,15 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import styles from "./searchBar.module.css";
 
 export default function SearchBar() {
-  return <input type="text" />;
+  return (
+    <div className={styles.searchWrapper}>
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Search for movies, TV shows..."
+      />
+      <MagnifyingGlassIcon className={styles.icon} />
+    </div>
+  );
 }
