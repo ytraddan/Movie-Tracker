ef3d50b40ff857ab32d81fb56fd5f1b7300e94db
ytraddan <1nightshelter@gmail.com>
2026-09-15 22:42:01 +0300

    add search using tanstack query

---
+++ src/components/searchBar/SearchDropdown.tsx
@@ -0,0 +1,37 @@
+import SearchResultItem from "./SearchResultItem";
+import { Movie } from "@/lib/tmdb-types";
+import styles from "./searchDropdown.module.css";
+
+interface SearchDropdownProps {
+  isLoading: boolean;
+  isError: boolean;
+  movies: Movie[];
+  onSelect: () => void;
+}
+
+export default function SearchDropdown({
+  isLoading,
+  isError,
+  movies,
+  onSelect,
+}: SearchDropdownProps) {
+  return (
+    <div className={styles.wrapper}>
+      {isLoading && <div className="">Loading...</div>}
+
+      {isError && <div className="">Failed to load results</div>}
+
+      {!isLoading && !isError && movies.length === 0 && (
+        <div className="">No results found</div>
+      )}
+
+      <ul className={styles.movieList}>
+        {movies.map((movie) => (
+          <li key={movie.id} onClick={onSelect}>
+            <SearchResultItem movie={movie} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/lib/api/movies.ts
@@ -0,0 +1,11 @@
+import { PaginatedMovies } from "@/lib/tmdb-types";
+
+export async function searchMovies(query: string): Promise<PaginatedMovies> {
+  const res = await fetch(`/api/search?query=${query}`);
+
+  if (!res.ok) {
+    throw new Error(`Failed to complete the search ${res.status}`);
+  }
+
+  return res.json();
+}
