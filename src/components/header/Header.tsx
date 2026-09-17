1043a95c8a6381fe690cd51e697bf3925a73f30c
ytraddan <1nightshelter@gmail.com>
2026-09-17 12:42:12 +0300

    refactor header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,34 @@
 import Link from "next/link";
 import styles from "./header.module.css";
 import SearchBar from "@/components/searchBar/SearchBar";
-import FavoriteButton from "@/components/favoriteButton/FavoriteButton";
+import { BookmarkIcon } from "@heroicons/react/24/solid";
+
 import Image from "next/image";
 
 export default function Header() {
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
-        <Link href="/" className={styles.title}>
+        <Link href="/" className={styles.logo}>
           <Image
-            className={styles.logo}
+            className={styles.logoIcon}
             src="/movie.svg"
             alt="Movie icon"
             width={30}
             height={30}
           />
-          <div>
-            <span>Movie </span>
+          <div className={styles.title}>
+            <span> Movie </span>
             <span className={styles.highlighted}>Tracker</span>
           </div>
         </Link>
+
         <SearchBar />
-        <FavoriteButton />
+
+        <Link href={"/collection"} className={styles.collectionButton}>
+          <span className={styles.collectionText}>Favorites</span>
+          <BookmarkIcon className={styles.bookmarkIcon} />
+        </Link>
       </header>
     </div>
   );
