63a6dd3dbaa5f6de1593ac1342a6717925033538
ytraddan <1nightshelter@gmail.com>
2026-09-18 18:48:21 +0300

    refactor movie details component

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,9 +1,11 @@
+import CastList from "@/components/movieDetails/CastList";
 import MovieDetails from "@/components/movieDetails/MovieDetails";
 import SimlarMovies from "@/components/movieDetails/SimilarMovies";
 import { SimilarMoviesSkeleton } from "@/components/skeletons/SimilarMoviesSkeleton";
 import { fetchMovieDetails } from "@/lib/tmdb";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
+import styles from "./page.module.css";
 
 interface MoviePageProps {
   params: { id: string };
@@ -21,11 +23,15 @@ export default async function MoviePage({ params }: MoviePageProps) {
   }
 
   return (
-    <>
-      <MovieDetails movie={movie} castLimit={CAST_LIMIT} />
+    <article className={styles.wrapper}>
+      <div>
+        <MovieDetails movie={movie} />
+        <CastList cast={movie.credits.cast} limit={CAST_LIMIT} />
+      </div>
+
       <Suspense fallback={<SimilarMoviesSkeleton itemsCount={SIMLAR_LIMIT} />}>
         <SimlarMovies movieId={id} limit={SIMLAR_LIMIT} />
       </Suspense>
-    </>
+    </article>
   );
 }
--- src/components/movieDetails/MovieDetails.tsx
+++ src/components/movieDetails/MovieDetails.tsx
@@ -2,7 +2,6 @@ import { IMAGE_SIZES } from "@/lib/constants";
 import { type MovieDetails } from "@/lib/tmdb-types";
 import { getImageUrl } from "@/lib/utils";
 import styles from "./movieDetails.module.css";
-import CastList from "./CastList";
 import MovieMeta from "./MovieMeta";
 import MovieActions from "../movieActions/MovieActions";
 import BackButton from "@/components/backButton/BackButton";
@@ -10,10 +9,9 @@ import ImageWithFallback from "../imageWithFallback/ImageWithFallback";
 
 interface MovieDetailsProps {
   movie: MovieDetails;
-  castLimit: number;
 }
 
-export default function MovieDetails({ movie, castLimit }: MovieDetailsProps) {
+export default function MovieDetails({ movie }: MovieDetailsProps) {
   const posterUrl = getImageUrl(movie.poster_path, IMAGE_SIZES.poster.original);
   const backdropUrl = getImageUrl(
     movie.backdrop_path,
@@ -21,48 +19,45 @@ export default function MovieDetails({ movie, castLimit }: MovieDetailsProps) {
   );
 
   return (
-    <article className={styles.wrapper}>
-      <div className={styles.hero}>
-        <div className={styles.backdropWrapper}>
+    <div className={styles.hero}>
+      <div className={styles.backdropWrapper}>
+        <ImageWithFallback
+          fallback="/backdrop-fallback.png"
+          className={styles.backdropImage}
+          src={backdropUrl}
+          alt={movie.title}
+          sizes="100vw"
+          loading="eager"
+          fill
+        />
+        <div className={styles.backdropOverlayLeft} />
+        <div className={styles.backdropOverlayRight} />
+        <div className={styles.backdropOverlayBottom} />
+      </div>
+
+      <BackButton />
+
+      <div className={styles.heroContent}>
+        <div className={styles.info}>
           <ImageWithFallback
-            fallback="/backdrop-fallback.png"
-            className={styles.backdropImage}
-            src={backdropUrl}
+            fallback="/poster-fallback.png"
+            className={styles.poster}
+            src={posterUrl}
             alt={movie.title}
-            sizes="100vw"
-            loading="eager"
-            fill
+            width={320}
+            height={480}
           />
-          <div className={styles.backdropOverlayLeft} />
-          <div className={styles.backdropOverlayRight} />
-          <div className={styles.backdropOverlayBottom} />
-        </div>
 
-        <BackButton />
-
-        <div className={styles.heroContent}>
-          <div className={styles.info}>
-            <ImageWithFallback
-              fallback="/poster-fallback.png"
-              className={styles.poster}
-              src={posterUrl}
-              alt={movie.title}
-              width={320}
-              height={480}
-            />
-
-            <div className={styles.metaWrapper}>
-              <MovieMeta movie={movie} />
-              <MovieActions movie={movie} />
-              <section className={styles.overview}>
-                <h2 className={styles.overviewTitle}>Overview</h2>
-                <p className={styles.overviewText}>{movie.overview}</p>
-              </section>
-            </div>
+          <div className={styles.metaWrapper}>
+            <MovieMeta movie={movie} />
+            <MovieActions movie={movie} />
+            <section className={styles.overview}>
+              <h2 className={styles.overviewTitle}>Overview</h2>
+              <p className={styles.overviewText}>{movie.overview}</p>
+            </section>
           </div>
         </div>
       </div>
-      <CastList cast={movie.credits.cast} limit={castLimit} />
-    </article>
+    </div>
   );
 }
