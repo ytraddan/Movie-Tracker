ffe5fd070f9670a0fa67c6aff02a4172f82a32fe
ytraddan <1nightshelter@gmail.com>
2026-09-20 14:00:37 +0300

    refactor no found page

--- src/app/movie/[id]/not-found.tsx
+++ src/app/movie/[id]/not-found.tsx
@@ -5,6 +5,9 @@ export default async function NotFound() {
   return (
     <div className={styles.notFound}>
       <h1 className={styles.title}>Movie not found</h1>
+      <p className={styles.message}>
+        The page you are looking for doesn&apos;t exist
+      </p>
       <BackButton />
     </div>
   );
