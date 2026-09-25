86689be931f63d8603ba8919dc4c8fc3420fcdb5
ytraddan <1nightshelter@gmail.com>
2026-09-25 15:59:02 +0300

    use unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://image.tmdb.org/**")],
+    unoptimized: true,
   },
 };
 
